053308c48a918873d3e10b4b400f3758dd82db3c
AndreiLhu <leahu.andreilucian@gmail.com>
2023-02-11 15:48:28 +0100

    Adding some style

--- components/TopLocation.jsx
+++ components/TopLocation.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const TopLocation = ({ cityName, cars }) => {
   return (
-    <div>
-      <h2>{cityName}</h2>
-      <p>{cars}</p>
+    <div class="flex flex-col bg-white mb-5 text-center">
+      <h2 class="text-3xl font-light">{cityName}</h2>
+      <p class="text-gray-500">{cars}</p>
     </div>
   );
 };
--- pages/location-search/index.jsx
+++ pages/location-search/index.jsx
@@ -8,16 +8,21 @@ import data from '@/data';
 function Location() {
   const session = useSession();
   return (
-    <div className="flex flex-col justify-between">
+    <div className="flex flex-col justify-between ml-5 mr-5">
       {session && <TopInfo />}
-      <h1>Find and rent cool cars in your town </h1>
-      <input type="search" placeholder="search" />
-      <h3>Top Location</h3>
+      <h1 class="text-4xl my-10  text-gray-600">
+        Find and rent cool cars in your town{' '}
+      </h1>
+
+      <input
+        class="rounded-full h-12 mb-10 text-center "
+        type="search"
+        placeholder="choose location"
+      />
+      <h3 class="mb-3 text-xl text-gray-600">Top Location</h3>
       {data.map((e) => (
         <div>
-          {' '}
-          <TopLocation cityName={e.carName} cars={e.doors} />
-          <TopLocation cityName={e.carName} cars={e.doors} />{' '}
+          <TopLocation cityName={e.location} cars={e.horsepower} />
         </div>
       ))}
 
