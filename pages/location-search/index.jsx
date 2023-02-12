0e80a8ed4e9dd428aedaea43d2aa714e91fead65
AndreiLhu <leahu.andreilucian@gmail.com>
2023-02-12 19:02:58 +0100

    fixed some bugs

--- pages/location-search/index.jsx
+++ pages/location-search/index.jsx
@@ -10,19 +10,19 @@ function Location() {
   return (
     <div className="flex flex-col justify-between ml-5 mr-5">
       {session && <TopInfo />}
-      <h1 class="text-4xl my-10  text-gray-600">
+      <h1 className="text-4xl my-10  text-gray-600">
         Find and rent cool cars in your town{' '}
       </h1>
 
       <input
-        class="rounded-full h-12 mb-10 text-center "
+        className="rounded-full h-12 mb-10 text-center "
         type="search"
         placeholder="choose location"
       />
       <h3 className="mb-3 text-xl text-gray-600">Top Location</h3>
       {data.map((e, index) => (
-        <div>
-          <TopLocation cityName={e.location} cars={e.horsepower} key={index} />
+        <div key={index}>
+          <TopLocation cityName={e.location} cars={e.horsepower} />
         </div>
       ))}
 
