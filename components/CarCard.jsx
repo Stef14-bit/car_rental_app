67d8da2216696a1debbb5f816f8e7995180fc5a9
AndreiLhu <leahu.andreilucian@gmail.com>
2023-02-12 19:18:34 +0100

    changing card colors

--- components/CarCard.jsx
+++ components/CarCard.jsx
@@ -13,7 +13,7 @@ const CarCard = ({
   price,
 }) => {
   return (
-    <div className="flex flex-col bg-gray-100 ml-5 mr-5 mb-10   border-4 border-indigo-500 shadow-lg shadow-indigo-500/75 ">
+    <div className="flex flex-col bg-gray-100 ml-5 mr-5 mb-10   border-4 border-gray-500 shadow-lg shadow-gray-500/75 ">
       <h2 className="text-3xl  ml-5 ">{category}</h2>
       <img src={carImage} className="max-w-xs m-4" />
       <div className="flex">
@@ -33,7 +33,7 @@ const CarCard = ({
         href="https://giphy.com/clips/justin-funny-dog-werk-jnbvNSt7xPPEiWfCPu"
         target="_blank"
       >
-        <button className=" bg-sky-400 w-2/3 h-10 rounded-full text-white font-semibold ml-14 mb-4 shadow-lg shadow-indigo-500/50">
+        <button className=" bg-sky-400 w-2/3 h-10 rounded-full text-white font-semibold ml-14 mb-4 shadow-lg ">
           Rent
         </button>
       </Link>
