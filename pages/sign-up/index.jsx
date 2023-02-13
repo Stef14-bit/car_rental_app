2b78c5345b9c2fdbed2f2e1453042721b7167dd8
AndreiLhu <leahu.andreilucian@gmail.com>
2023-02-13 13:30:41 +0100

    Adding auto redirect to profile

--- pages/sign-up/index.jsx
+++ pages/sign-up/index.jsx
@@ -2,10 +2,19 @@ import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 import MobileNavbar from '@/components/MobileNavbar';
 import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const Home = () => {
   const session = useSession();
   const supabase = useSupabaseClient();
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/profile');
+    }, 2000);
+  }, []);
 
   return (
     <div className="">
@@ -19,8 +28,11 @@ const Home = () => {
           />
         </div>
       ) : (
-        <Link href="/profile">Redirect to profile</Link>
+        <Link className="text-2xl text-gray-600 " href="/profile">
+          Redirect to profile...
+        </Link>
       )}
+
       <MobileNavbar />
     </div>
   );
