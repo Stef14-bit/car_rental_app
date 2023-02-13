474e38a9b8d60c75f0453d8c58f61b1fd283e243
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-02-13 22:07:35 +0100

    get from database carlist query

--- pages/car-search/car-list/index.jsx
+++ pages/car-search/car-list/index.jsx
@@ -1,27 +1,57 @@
-import CarCard from '@/components/CarCard';
-import React from 'react';
-import data from '@/data';
+import CarCard from "@/components/CarCard";
+import React, { useEffect, useState } from "react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import TopInfo from "@/components/TopInfo";
 
 const CarList = () => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getCars();
+  }, []);
+
+  async function getCars() {
+    try {
+      setLoading(true);
+      let { data, error, status } = await supabase.from("cars").select("*");
+      if (error && status !== 406) {
+        throw error;
+      }
+      if (data) {
+        setData(data);
+      }
+    } catch (error) {
+      console.error(error, "Error loading cars");
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <div>
-      {data.map((el, index) => (
-        <div key={index}>
-          <CarCard
-            category={el.category}
-            carImage={
-              'https://seeklogo.com/images/L/line-art-car-logo-99DBB62CB3-seeklogo.com.png'
-            }
-            make={el.make}
-            model={el.model}
-            rate={el.rate}
-            transmission={el.transmisson}
-            doors={el.doors}
-            horsepower={el.horsepower}
-            price={el.price}
-          />
-        </div>
-      ))}
+      <TopInfo />
+      {loading ? (
+        <h3>Loading ...</h3>
+      ) : (
+        data.map((e, index) => (
+          <div key={index}>
+            <CarCard
+              category={e.category}
+              carImage={
+                "https://seeklogo.com/images/L/line-art-car-logo-99DBB62CB3-seeklogo.com.png"
+              }
+              make={e.make}
+              model={e.model}
+              rate={e.rate}
+              transmission={"e.transmisson"}
+              doors={e.doors}
+              horsepower={e.horsepower}
+              price={e.price}
+            />
+          </div>
+        ))
+      )}
     </div>
   );
 };
