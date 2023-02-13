eed73dc441cc23b667e2f34cd11fc872bbb5f698
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-02-13 23:22:58 +0100

    fixed icon size

--- components/MobileNavbar.jsx
+++ components/MobileNavbar.jsx
@@ -8,16 +8,16 @@ function MobileNavbar() {
   return (
     <div className="bg-slate-400 absolute bottom-0 left-0 flex justify-around items-center w-full h-20 ">
       <Link href="/location-search">
-        <GrLocation />
+        <GrLocation size={"1.5em"} />
       </Link>
       <Link href="/car-search">
-        <BsSearch />
+        <BsSearch size={"1.5em"} />
       </Link>
       <Link href="/favorites">
-        <BsStar />
+        <BsStar size={"1.5em"} />
       </Link>
       <Link href="/profile">
-        <CgProfile />
+        <CgProfile size={"1.5em"} />
       </Link>
     </div>
   );
