3ad7f1bbff4595797d17a5953f8c9bf1586b212c
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-02-15 14:21:32 +0100

    car card doors fix

--- components/CarCard.jsx
+++ components/CarCard.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const CarCard = ({
   category,
@@ -23,16 +23,15 @@ const CarCard = ({
       <p className="text-gray-500 text-right mr-10 ">rating: {rate} </p>
       <div className="flex text-center">
         <p className="ml-12">{transmission}</p>
-        <p className="ml-24">{doors}</p>
+        <p className="ml-24">{doors} doors</p>
       </div>
       <p className=" text-center">{horsepower} Horsepower</p>
       <h3 className="text-xl text-center mb-2">
-        Starting from {price} € / Day{' '}
+        Starting from {price} € / Day{" "}
       </h3>
       <Link
         href="https://giphy.com/clips/justin-funny-dog-werk-jnbvNSt7xPPEiWfCPu"
-        target="_blank"
-      >
+        target="_blank">
         <button className=" bg-sky-400 w-2/3 h-10 rounded-full text-white font-semibold ml-14 mb-4 shadow-lg ">
           Rent
         </button>
