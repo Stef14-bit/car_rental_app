4599eb4cb593933646c259671507b1d9ceff0209
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-02-17 12:13:23 +0100

    style fix atempt 3

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,8 @@ function MyApp({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
 
   return (
-    <div className="p-0 m-0 grid grid-rows-1 h-screen max-w-screen bg-gradient-to-b from-slate-400 to-slate-200 overflow-hidden">
-      <div className=" overflow-auto pb-10">
+    <div className="p-0 m-0 grid grid-rows-1  h-screen max-w-screen bg-gradient-to-b from-slate-400 to-slate-200 overflow-hidden">
+      <div className=" overflow-auto ">
         {" "}
         <SessionContextProvider
           supabaseClient={supabase}
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
           <Component {...pageProps} />
         </SessionContextProvider>
       </div>
-      <div className="w-full sticky bottom-auto">
+      <div className="">
         <MobileNavbar />
       </div>
     </div>
