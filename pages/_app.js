370937294ed20a85c60c8474d48e81ede58db5e6
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-02-17 21:31:15 +0100

    added footer

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,21 @@ import "../styles/globals.css";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
-import Example from "@/components/Navbar";
+import NavBar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 function MyApp({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
 
   return (
-    <div className="h-screen  bg-gradient-to-b from-gray to-white">
-      {" "}
-      <Example />
+    <div className="min-h-screen flex flex-col content-center justify-between bg-gradient-to-b from-gray to-white">
+      <NavBar />
       <SessionContextProvider
         supabaseClient={supabase}
         initialSession={pageProps.initialSession}>
         <Component {...pageProps} />
       </SessionContextProvider>
+      <Footer />
     </div>
   );
 }
