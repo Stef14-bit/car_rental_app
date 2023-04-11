8b9616d6e2a0aedd9c95213949baf2c96fe39c5b
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-04-11 22:01:43 +0200

    homepage completed

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,35 @@
-import Link from "next/link";
-import useQuery from "@/hooks/useQuery";
-import CarCard from "@/components/CarCard";
-
+import CategoryCard from "@/components/CategoryCard";
+import suv from "../assets/suv.jpg";
+import sedan from "../assets/sedan.jpg";
+import sport from "../assets/sport.jpg";
+import coupe from "../assets/coupe.jpg";
+import old from "../assets/old.jpg";
+import luxury from "../assets/luxury.jpg";
 const Home = () => {
-  const { data, loading } = useQuery("cars", "*");
-
   return (
-    <div className="flex flex-col h-full relative top-16">
+    <div className="flex flex-col h-full  top-16 ">
       <h1 className="text-5xl font-bold text-darkGray p-4 ">
         Rent cool cars in your city
       </h1>
       <h2 className="text-center  text-darkGray py-12">
         Curently availible in {"city"}
       </h2>
-
-      {loading ? (
-        <h3>Loading ...</h3>
-      ) : (
-        data.map((e, index) => (
-          <div key={index}>
-            <CarCard
-              category={e.category}
-              carImage={
-                "https://seeklogo.com/images/L/line-art-car-logo-99DBB62CB3-seeklogo.com.png"
-              }
-              make={e.make}
-              model={e.model}
-              rate={e.rate}
-              transmission={e.transmisson}
-              doors={e.doors}
-              horsepower={e.horsepower}
-              price={e.price}
-            />
-          </div>
-        ))
-      )}
+      <div className="w-fit flex flex-wrap justify-center">
+        <CategoryCard categoryImg={suv} category={"SUV"} page={"suv"} />
+        <CategoryCard categoryImg={sedan} category={"Sedan"} page={"sedan"} />
+        <CategoryCard categoryImg={sport} category={"Sport"} page={"sport"} />
+        <CategoryCard categoryImg={coupe} category={"Coupe"} page={"coupe"} />
+        <CategoryCard
+          categoryImg={old}
+          category={"Oldtimer"}
+          page={"oldtimer"}
+        />
+        <CategoryCard
+          categoryImg={luxury}
+          category={"Luxury"}
+          page={"luxury"}
+        />
+      </div>
     </div>
   );
 };
