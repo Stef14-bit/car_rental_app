30b3fb989cbe2dd1842b3028ffb147bf78ffbd08
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-04-25 22:09:33 +0200

    basic input for date picker

--- pages/rentPage.jsx
+++ pages/rentPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function rentPage() {
-  return <div>rentPage</div>;
+  return (
+    <div>
+      <form className="w-full flex flex-col">
+        <input
+          className="h-12 w-3/4 rounded-md text-center bg-blue text-white font text-lg"
+          type="datetime-local"
+        />{" "}
+        <input
+          className="h-12 w-3/4 rounded-md text-center bg-blue text-slate-50 font text-lg"
+          type="datetime-local"
+        />{" "}
+      </form>
+    </div>
+  );
 }
 
 export default rentPage;
