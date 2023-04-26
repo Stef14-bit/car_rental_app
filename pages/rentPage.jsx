bd7ce880d20b19707b09fb2f62d4e5f7c1d705dc
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-04-26 17:39:13 +0200

    rent page style

--- pages/rentPage.jsx
+++ pages/rentPage.jsx
@@ -2,16 +2,19 @@ import React from "react";
 
 function rentPage() {
   return (
-    <div>
-      <form className="w-full flex flex-col">
+    <div className="flex justify-center items-center h-full">
+      <form className="w-96 flex flex-col items-center">
+        <label htmlFor="from">From</label>
         <input
-          className="h-12 w-3/4 rounded-md text-center bg-blue text-white font text-lg"
+          id="from"
+          className="h-12 w-full rounded-md text-center bg-blue text-white font text-lg mb-4"
           type="datetime-local"
-        />{" "}
+        />
+        <label htmlFor="to">To</label>
         <input
-          className="h-12 w-3/4 rounded-md text-center bg-blue text-slate-50 font text-lg"
+          className="h-12 w-full rounded-md text-center bg-blue text-slate-50 font text-lg"
           type="datetime-local"
-        />{" "}
+        />
       </form>
     </div>
   );
