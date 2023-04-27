a2b8ee9d4a81f8902b9079b351f660b50cd08e69
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-04-27 20:23:52 +0200

    rent car page and rent form complete

--- pages/rentPage.jsx
+++ pages/rentPage.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function rentPage() {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "from") {
+      setStartDate(value);
+    }
+    if (id === "to") {
+      setEndDate(value);
+    }
+  };
+
+  const handleSubmit = () => {
+    console.log(startDate, endDate);
+  };
   return (
     <div className="flex justify-center items-center h-full">
-      <form className="w-96 flex flex-col items-center">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-96  h-96 flex flex-col items-center justify-around">
         <label htmlFor="from">From</label>
         <input
           id="from"
+          value={startDate}
+          onChange={(e) => handleInputChange(e)}
           className="h-12 w-full rounded-md text-center bg-blue text-white font text-lg mb-4"
-          type="datetime-local"
+          type="date"
         />
         <label htmlFor="to">To</label>
         <input
+          id="to"
+          value={endDate}
+          onChange={(e) => handleInputChange(e)}
           className="h-12 w-full rounded-md text-center bg-blue text-slate-50 font text-lg"
-          type="datetime-local"
+          type="date"
         />
+
+        <button type="Submit" onClick={handleSubmit}>
+          Rent Car
+        </button>
       </form>
     </div>
   );
