b71d2f876e33cc1d32f8c5a5e930ed07ef852ead
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-05-03 21:16:47 +0200

    build

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="flex justify-center items-center h-16 bg-gradient-to-tr from-sky-900 via-sky-600 to-green-800">
-      Footer
+      Footer--
     </div>
   );
 }
