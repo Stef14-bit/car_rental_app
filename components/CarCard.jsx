32227bd4e016200ce5ff257eb24f9603e7216494
Stefan.Iv PC <stefan.ivanus@outlook.de>
2023-05-06 00:12:32 +0200

    rent modal implemented

--- components/CarCard.jsx
+++ components/CarCard.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import RentModal from "./RentModal";
 
 const CarCard = ({
   id,
@@ -12,30 +13,55 @@ const CarCard = ({
   doors,
   horsepower,
   price,
-  onClick,
 }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleRentClick = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div className="flex flex-col w-72 border m-5 rounded-md shadow-lg hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-      <Link href={`/cars/${id}`}>
-        <img className="h-48 w-full object-cover" src={carImage} alt="car" />
-      </Link>
-      <div className="flex flex-col p-5">
-        <h2 className="text-xl font-semibold mb-2">
-          {make} {model}
-        </h2>
-        <p>Category: {category}</p>
-        <p>Rate: {rate}</p>
-        <p>Transmission: {transmission}</p>
-        <p>Doors: {doors}</p>
-        <p>Horsepower: {horsepower}</p>
-        <p>Price: {price} $/day</p>
-        <Link href={`/rent-page/${id}`}>
-          <button className="bg-gradient-to-tr from-sky-900 via-sky-600 to-green-800 h-10 my-5 rounded-md">
+    <>
+      <div className="flex flex-col w-72 border m-5 rounded-md shadow-lg hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+        <Link href={`/cars/${id}`}>
+          <img className="h-48 w-full object-cover" src={carImage} alt="car" />
+        </Link>
+        <div className="flex flex-col p-5">
+          <h2 className="text-xl font-semibold mb-2">
+            {make} {model}
+          </h2>
+          <p>Category: {category}</p>
+          <p>Rate: {rate}</p>
+          <p>Transmission: {transmission}</p>
+          <p>Doors: {doors}</p>
+          <p>Horsepower: {horsepower}</p>
+          <p>Price: {price} $/day</p>
+          <button
+            className="bg-gradient-to-tr from-sky-900 via-sky-600 to-green-800 h-10 my-5 rounded-md"
+            onClick={handleRentClick}>
             Rent
           </button>
-        </Link>
+        </div>
       </div>
-    </div>
+
+      <RentModal
+        showModal={showModal}
+        handleModalClose={handleModalClose}
+        id={id}
+        category={category}
+        make={make}
+        model={model}
+        rate={rate}
+        transmission={transmission}
+        doors={doors}
+        horsepower={horsepower}
+        price={price}
+      />
+    </>
   );
 };
 
--- components/RentModal.jsx
+++ components/RentModal.jsx
@@ -71,7 +71,7 @@ const RentModal = ({
             </div>
             <div className="flex justify-end mt-5">
               <button
-                className="bg-gray-400 hover:bg-gray-500 text-white px-4 py-2 rounded-md mr-2"
+                className="bg-gradient-to-tr from-sky-900 via-sky-600 to-green-800 text-white px-4 py-2 rounded-md mr-2"
                 onClick={handleCancelClick}>
                 Cancel
               </button>
